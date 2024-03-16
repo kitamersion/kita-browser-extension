55c1d94c89379df0722397694940ef346c5f7419
HelpfulFish <learning_open_source_dev@proton.me>
2024-03-16 17:15:17 +1300

    feat: open drawer for updating

--- src/pages/popup/components/updateVideo.tsx
+++ src/pages/popup/components/updateVideo.tsx
@@ -2,8 +2,23 @@ import eventbus from "@/api/eventbus";
 import { VIDEO_UPDATED_BY_ID } from "@/data/events";
 import { IVideo, SiteKey } from "@/types/video";
 import { convertToSeconds, formatDuration } from "@/utils";
-import { Button, FormControl, FormLabel, Input, Select } from "@chakra-ui/react";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  FormControl,
+  FormLabel,
+  IconButton,
+  Input,
+  Select,
+  useDisclosure,
+} from "@chakra-ui/react";
 import React, { useState, useEffect, useMemo } from "react";
+import { MdEdit } from "react-icons/md";
 
 const UpdateVideo = ({ id, origin, video_duration, video_title, created_at, video_url, tags }: IVideo) => {
   const initialState = useMemo(() => {
@@ -19,10 +34,17 @@ const UpdateVideo = ({ id, origin, video_duration, video_title, created_at, vide
   }, [created_at, id, origin, tags, video_duration, video_title, video_url]);
   const [video, setVideo] = useState(initialState);
   const [isChanged, setIsChanged] = useState(false);
+  const [isInvalid, setIsInvalid] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     e.preventDefault();
     const { name, value } = e.target;
+    if (!value) {
+      setIsInvalid(true);
+    } else {
+      setIsInvalid(false);
+    }
     setVideo({
       ...video,
       [name]: value,
@@ -41,36 +63,49 @@ const UpdateVideo = ({ id, origin, video_duration, video_title, created_at, vide
       video_duration: convertToSeconds(video.video_duration),
     };
     eventbus.publish(VIDEO_UPDATED_BY_ID, { message: "Updating video", value: updatedVideo });
+    onClose();
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <FormControl id="video_title">
-        <FormLabel>Video Title</FormLabel>
-        <Input name="video_title" value={video.video_title} onChange={handleChange} />
-      </FormControl>
-      <FormControl id="video_url">
-        <FormLabel>Video URL</FormLabel>
-        <Input name="video_url" value={video.video_url} onChange={handleChange} />
-      </FormControl>
-      <FormControl id="video_duration">
-        <FormLabel>Video Duration</FormLabel>
-        <Input name="video_duration" value={video.video_duration} onChange={handleChange} />
-      </FormControl>
-      <FormControl id="origin">
-        <FormLabel>Origin</FormLabel>
-        <Select name="origin" value={video.origin} onChange={handleChange}>
-          {Object.values(SiteKey).map((siteKey) => (
-            <option key={siteKey} value={siteKey}>
-              {siteKey}
-            </option>
-          ))}
-        </Select>
-      </FormControl>
-      <Button mt={4} type="submit" isDisabled={!isChanged}>
-        Update Video
-      </Button>
-    </form>
+    <>
+      <IconButton icon={<MdEdit />} aria-label="Edit item" variant="ghost" rounded="full" title="Edit item" onClick={onOpen} />
+      <Drawer onClose={onClose} isOpen={isOpen} size={"full"} placement={"bottom"}>
+        <DrawerOverlay />
+        <DrawerContent p="6" background={"gray.800"}>
+          <DrawerCloseButton />
+          <DrawerHeader>Editing</DrawerHeader>
+          <DrawerBody>
+            <form onSubmit={handleSubmit}>
+              <FormControl id="video_title">
+                <FormLabel>Video Title</FormLabel>
+                <Input isInvalid={!video.video_title} name="video_title" value={video.video_title} onChange={handleChange} />
+              </FormControl>
+              <FormControl id="video_url">
+                <FormLabel>Video URL</FormLabel>
+                <Input isInvalid={!video.video_url} name="video_url" value={video.video_url} onChange={handleChange} />
+              </FormControl>
+              <FormControl id="video_duration">
+                <FormLabel>Video Duration</FormLabel>
+                <Input isInvalid={!video.video_duration} name="video_duration" value={video.video_duration} onChange={handleChange} />
+              </FormControl>
+              <FormControl id="origin">
+                <FormLabel>Origin</FormLabel>
+                <Select isInvalid={!video.origin} name="origin" value={video.origin} onChange={handleChange}>
+                  {Object.values(SiteKey).map((siteKey) => (
+                    <option key={siteKey} value={siteKey}>
+                      {siteKey}
+                    </option>
+                  ))}
+                </Select>
+              </FormControl>
+              <Button mt={4} type="submit" isDisabled={!isChanged || isInvalid}>
+                Update Video
+              </Button>
+            </form>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 };
 
--- src/pages/popup/components/videoItem.tsx
+++ src/pages/popup/components/videoItem.tsx
@@ -6,9 +6,10 @@ import React from "react";
 import eventbus from "@/api/eventbus";
 import { VIDEO_DELETED_BY_ID } from "@/data/events";
 import OriginToIcon from "./originToIcon";
-import { IoMdLink } from "react-icons/io";
+import UpdateVideo from "./updateVideo";
 
-const VideoItem = ({ id, created_at, origin, video_duration, video_title, video_url }: IVideo) => {
+const VideoItem = (video: IVideo) => {
+  const { id, created_at, origin, video_duration, video_title, video_url } = video;
   const handleDeleteById = (id: string) => {
     eventbus.publish(VIDEO_DELETED_BY_ID, { message: `Delete video ${id}`, value: { id: id } });
   };
@@ -22,18 +23,20 @@ const VideoItem = ({ id, created_at, origin, video_duration, video_title, video_
               <OriginToIcon siteKey={origin} />
               <Link p={2} href={video_url} isExternal display={"flex"} gap={1} alignItems={"center"}>
                 <Text as="b">{video_title}</Text>
-                <IoMdLink />
               </Link>
             </Flex>
-            <IconButton
-              icon={<MdDelete />}
-              aria-label="Delete item"
-              colorScheme="red"
-              variant="ghost"
-              rounded="full"
-              title={`Delete item ${id}`}
-              onClick={() => handleDeleteById(id)}
-            />
+            <Flex gap="1">
+              <UpdateVideo {...video} />
+              <IconButton
+                icon={<MdDelete />}
+                aria-label="Delete item"
+                colorScheme="red"
+                variant="ghost"
+                rounded="full"
+                title={`Delete item ${id}`}
+                onClick={() => handleDeleteById(id)}
+              />
+            </Flex>
           </Flex>
           <Text>Duration: {formatDuration(video_duration)}</Text>
           <Text>Created At: {formatTimestamp(created_at)}</Text>
