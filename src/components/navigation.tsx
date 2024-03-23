208e1dea1616cada3ad9cde0b9f06517d818b6da
HelpfulFish <learning_open_source_dev@proton.me>
2024-03-23 16:42:44 +1300

    feat: remove navigation secondary color

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,28 +1,17 @@
 import React from "react";
-import { Flex, Heading, useColorMode } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
 import { ActionItems } from "@/data/routes";
 import { nanoid } from "nanoid";
 import { TITLE } from "@/data/contants";
 import IsApplicationEnabledToggle from "@/pages/popup/components/isApplicationEnabledToggle";
 
 const Navigation = () => {
-  const { colorMode } = useColorMode();
   const renderActionItems = () => {
     return ActionItems.map((action) => action.component && <action.component key={nanoid()} />);
   };
 
   return (
-    <Flex
-      as="nav"
-      align="center"
-      alignContent="center"
-      justify="space-between"
-      m={3}
-      py={2}
-      px={6}
-      background={colorMode === "dark" ? "gray.900" : "gray.400"}
-      rounded={"full"}
-    >
+    <Flex as="nav" align="center" alignContent="center" justify="space-between" m={3} py={2} px={6}>
       <Flex alignItems={"center"} gap={2}>
         <Heading as={"h1"} fontSize="medium">
           {TITLE}
