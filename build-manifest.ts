2915e5ef919dea4016b68c47659eb4c1c02f053b
HelpfulFish <learning_open_source_dev@proton.me>
2024-04-02 20:38:45 +1300

    feat: build chrome and firefox V3 manifest

---
+++ build-manifest.ts
@@ -0,0 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require("fs");
+
+const PACKAGE_PATH = "./package.json";
+const EXTENSION_PATH = "./ext/manifest.json";
+const FIREFOX = "firefox";
+const CHROME = "chrome";
+
+const packageJson = JSON.parse(fs.readFileSync(PACKAGE_PATH, "utf8"));
+let manifest = fs.readFileSync(EXTENSION_PATH, "utf8");
+
+manifest = JSON.parse(manifest);
+
+if (process.env.TARGET_BROWSER === FIREFOX) {
+  manifest.browser_specific_settings = {
+    gecko: {
+      id: packageJson["browser-extension"]["firefox"]["id"],
+    },
+  };
+} else if (process.env.TARGET_BROWSER === CHROME) {
+  delete manifest.browser_specific_settings;
+}
+
+manifest = JSON.stringify(manifest, null, 4);
+
+fs.writeFileSync(EXTENSION_PATH, manifest);
