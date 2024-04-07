441cf3a7228dc58b037c342db5c541e7bdc3c442
HelpfulFish <learning_open_source_dev@proton.me>
2024-04-07 12:14:32 +1200

    feat: show toast bottom right

--- src/context/toastNotificationContext.tsx
+++ src/context/toastNotificationContext.tsx
@@ -38,7 +38,7 @@ export const ToastProvider: React.FC = ({ children }) => {
       status: status,
       duration: duration ?? TOAST_RESET_DELAY_MS,
       isClosable: true,
-      position: ENV === "dev" ? "bottom" : "bottom-left",
+      position: "bottom-right",
       variant: "subtle",
     });
   };
@@ -48,7 +48,7 @@ export const ToastProvider: React.FC = ({ children }) => {
       ...messages.loading,
       status: "loading",
       duration: null,
-      position: "bottom-left",
+      position: "bottom-right",
       variant: "subtle",
     };
     const loadingToastId = chakraToast(loadingToast);
