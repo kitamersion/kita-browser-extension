d872280b21ac59ae3396eef9e8cf5f8c7e8943c3
HelpfulFish <learning_open_source_dev@proton.me>
2024-04-29 20:29:53 +1200

    chore: disclaimers

--- src/pages/settings/tabs/integrationTab.tsx
+++ src/pages/settings/tabs/integrationTab.tsx
@@ -1,4 +1,4 @@
-import { Grid, TabPanel } from "@chakra-ui/react";
+import { Center, Flex, Grid, TabPanel, Text } from "@chakra-ui/react";
 import React, { Suspense } from "react";
 import LoadingState from "@/components/states/LoadingState";
 import Anilist from "../integrations/anilist";
@@ -13,6 +13,11 @@ const IntegrationTab = () => {
         <Grid templateColumns={`repeat(${columns}, 1fr)`} gap={4} mt={4} mx={2}>
           <Anilist />
         </Grid>
+        <Center mt={6}>
+          <Text fontSize={14} color={"tomato"}>
+            Please note that Kitamersion is an independent entity and is not affiliated with any of the integrations displayed on this page.
+          </Text>
+        </Center>
       </Suspense>
     </TabPanel>
   );
